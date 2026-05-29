96861246cd45897e7248ce94fc99d6e370f1d50c
Ajitofy <97536539+Ajitofy@users.noreply.github.com>
2026-05-30 02:57:59 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+        l=r=0
+        res=[]
+        while l<len(temp):
+            while l<len(temp) and temp[l]>=temp[r]:
+                if r==len(temp)-1:
+                    res.append(0)
+                    l+=1
+                    r=l                        
+                else:
+                    r+=1
+            if l<len(temp) and temp[l]<temp[r]:
+                res.append(r-l)
+                l+=1
+                r=l+1
+                if l==len(temp)-1:
+                    res.append(0)
+                    return res
+        return res
+            
